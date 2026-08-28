8739383a4d692e303c2dd9abde60b24836d02374
Pomboverso <github.5yeil@simplelogin.fr>
2026-08-27 18:26:07 -0700

    adding alert and spinner
    - fix output and command overflow
    - add scrollbar styles

---
+++ src/components/spinner.vue
@@ -0,0 +1,32 @@
+<template>
+  <span class="spinner"></span>
+</template>
+
+<style scoped>
+.spinner {
+  &::before {
+    content: 'Thinking ';
+  }
+  &:after {
+    content: '/';
+    display: inline-block;
+    width: 1ch;
+    animation: spinner 600ms infinite;
+  }
+}
+
+@keyframes spinner {
+  0% {
+    content: '/';
+  }
+  25% {
+    content: '-';
+  }
+  50% {
+    content: '\\';
+  }
+  75% {
+    content: '|';
+  }
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,12 +1,26 @@
 <script setup>
+import Spinner from '@/components/spinner.vue'
 import TextBubble from '@/components/text-bubble.vue'
+import Alert from '@/components/alert.vue'
 </script>
 
 <template>
   <section class="home">
+    <h1>Chat Geppetto</h1>
     <div class="output">
       <TextBubble class="me">Hola</TextBubble>
-      <TextBubble>Hola</TextBubble>
+      <TextBubble><Spinner /></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
+      <TextBubble><Alert class="error">Error!</Alert></TextBubble>
     </div>
     <div class="input">
       <p contenteditable="true" id="command"></p>
